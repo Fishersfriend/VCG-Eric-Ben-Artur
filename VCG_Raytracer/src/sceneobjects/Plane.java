0b5c5ae2c8cce5a859bb03d3e4aa122fdd23546c
northernLights90 <artur.baranowski@gmx.de>
2016-06-14 11:45:29 +0200

    MatrixTransformation für die Plane gemacht!!! :):)

--- VCG_Raytracer/src/sceneobjects/Plane.java
+++ VCG_Raytracer/src/sceneobjects/Plane.java
@@ -15,14 +15,44 @@ public class Plane extends Shape {
         super(position, material);
         this.normal = normal.normalize();
 
-
+        matrixTransformation = new Matrix4().scale(1).translate(position);
+        inverse = matrixTransformation.invert();
     }
 
     @Override
     public Vec3 intersect (Ray ray) {
-        float pnp0 = normal.scalar(ray.startPoint);
+        transfStart = ray.startPoint;
+        transfDirect = ray.direction;
 
-        //t = -((pnp0 + position.length())/(normal.scalar(ray.direction)));
+        transfStart = this.inverse.multVec3(transfStart,true);
+        transfDirect = this.inverse.multVec3(transfDirect,false).normalize();
+
+        float pnp0 = normal.scalar(transfStart);
+
+        if(normal.scalar(transfDirect) < 0){
+            ray.t = -((pnp0)/(normal.scalar(transfDirect)));
+            this.intersectionPoint = transfStart.add(transfDirect.multScalar(ray.t));
+            this.intersectionPoint = this.matrixTransformation.multVec3(intersectionPoint, true);
+            return intersectionPoint;
+
+        }
+
+        else
+        {
+            ray.t = -1;
+            return null;
+        }
+    }
+
+    /*
+    public Vec3 intersect (Ray ray) {
+        transfStart = ray.startPoint;
+        transfDirect = ray.direction;
+
+        transfStart = this.inverse.multVec3(transfStart,true);
+        transfDirect = this.inverse.multVec3(transfDirect,false).normalize();
+
+        float pnp0 = normal.scalar(transf);
 
         if(normal.scalar(ray.direction) < 0){
             ray.t = -((pnp0 + position.length())/(normal.scalar(ray.direction)));
@@ -37,10 +67,9 @@ public class Plane extends Shape {
             return null;
         }
     }
+    */
 
     public Vec3 getNormal (Ray ray, Vec3 intersectionPoint) {
         return normal;
     }
-
-
 }
